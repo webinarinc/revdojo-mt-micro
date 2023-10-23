2553fd716a7986e1b85c790f440d143db77128af
kylynangel15 <61867883+kylynangel15@users.noreply.github.com>
2023-10-24 04:06:19 +0800

    fix revdojo service provider

--- src/Providers/RevdojoMTServiceProvider.php
+++ src/Providers/RevdojoMTServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Config;
 use PDO;
 use Revdojo\MT\Console\RevdojoMTInstall;
+use Revdojo\MT\Providers\DatabaseServiceProvider;
+use Revdojo\MT\Middleware\CheckForMaintenance;
+
 class RevdojoMTServiceProvider extends ServiceProvider
 {
     /**
@@ -18,6 +21,7 @@ class RevdojoMTServiceProvider extends ServiceProvider
                 RevdojoMTInstall::class,
             ]);
         }
+        $this->app->register(DatabaseServiceProvider::class);
     }
 
     /**
@@ -25,6 +29,8 @@ class RevdojoMTServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       
+        $this->app['router']->pushMiddlewareToGroup('web', CheckForMaintenance::class);
+        $this->app['router']->pushMiddlewareToGroup('api', CheckForMaintenance::class);
+
     }
 }
