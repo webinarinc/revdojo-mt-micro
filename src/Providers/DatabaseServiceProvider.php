6dd5a113206bc3948017c896bbd11de8255fcf5a
kylynangel15 <61867883+kylynangel15@users.noreply.github.com>
2023-11-06 02:34:50 +0800

    2.0.21 | fix sp

--- src/Providers/DatabaseServiceProvider.php
+++ src/Providers/DatabaseServiceProvider.php
@@ -40,38 +40,42 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!config('database.connections.mysql_base_service')) {
-            return;
-        }
-        $services = Service::all();
+        try {
+            if (!config('database.connections.mysql_base_service')) {
+                return;
+            }
+            $services = Service::all();
 
-        $services->map(function ($service) {
-            $databaseConnection = [
-                'driver' => 'mysql',
-                'url' => env('DATABASE_URL'),
-                'host' => env('DB_HOST', '127.0.0.1'),
-                'port' => env('DB_PORT', '3306'),
-                'database' => $service->database_name,
-                'username' => env('DB_USERNAME', 'forge'),
-                'password' => env('DB_PASSWORD', ''),
-                'unix_socket' => env('DB_SOCKET', ''),
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
-                'prefix_indexes' => true,
-                'strict' => true,
-                'engine' => null,
-                'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                ]) : [],
-            ];
-            Config::set("database.connections.$service->database_connection", $databaseConnection);
-        });
+            $services->map(function ($service) {
+                $databaseConnection = [
+                    'driver' => 'mysql',
+                    'url' => env('DATABASE_URL'),
+                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'port' => env('DB_PORT', '3306'),
+                    'database' => $service->database_name,
+                    'username' => env('DB_USERNAME', 'forge'),
+                    'password' => env('DB_PASSWORD', ''),
+                    'unix_socket' => env('DB_SOCKET', ''),
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                    'prefix' => '',
+                    'prefix_indexes' => true,
+                    'strict' => true,
+                    'engine' => null,
+                    'options' => extension_loaded('pdo_mysql') ? array_filter([
+                        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                    ]) : [],
+                ];
+                Config::set("database.connections.$service->database_connection", $databaseConnection);
+            });
 
-        
-        if (config('revdojo-mt.service_system_id')) {
-            $myService = $services->where('system_id', config('revdojo-mt.service_system_id'))->first();
-            Config::set("database.default", $myService->database_connection);
+            
+            if (config('revdojo-mt.service_system_id')) {
+                $myService = $services->where('system_id', config('revdojo-mt.service_system_id'))->first();
+                Config::set("database.default", $myService->database_connection);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
     }
 }
--- src/Providers/SubDomainServiceProvider.php
+++ src/Providers/SubDomainServiceProvider.php
@@ -20,24 +20,29 @@ class SubDomainServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!config('database.connections.mysql_tenant_service')) {
+        try {
+
+            if (!config('database.connections.mysql_tenant_service')) {
+                return;
+            }
+            
+            $url = request()->root();
+            $parsedUrl = parse_url($url);
+
+            if (!isset($parsedUrl['host'])) {
+            return;
+            }
+
+            preg_match('/^(.*?)\./', $parsedUrl['host'], $matches);
+            $subdomain = empty($matches) ? null : $matches[1];
+            
+            $tenant = TenantBase::where('id', $subdomain)->first();
+
+            Config::set('tenancy.tenant', $tenant);
+            Config::set('tenancy.tenant_db', $tenant ? $tenant->tenancy_db_name : 'base_service');
+
+        } catch (\Throwable $th) {
             return;
         }
-        
-        $url = request()->root();
-        $parsedUrl = parse_url($url);
-
-        if (!isset($parsedUrl['host'])) {
-          return;
-        }
-
-        preg_match('/^(.*?)\./', $parsedUrl['host'], $matches);
-        $subdomain = empty($matches) ? null : $matches[1];
-        
-        $tenant = TenantBase::where('id', $subdomain)->first();
-
-        Config::set('tenancy.tenant', $tenant);
-        Config::set('tenancy.tenant_db', $tenant ? $tenant->tenancy_db_name : 'base_service');
-
     }
 }
