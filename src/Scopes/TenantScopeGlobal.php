49fa470dda60ed43dfb18cedae62c7033b8d2f91
kylynangel15 <61867883+kylynangel15@users.noreply.github.com>
2023-11-16 04:04:48 +0800

    2.0.30 | fix tenant scope

--- src/Scopes/TenantScopeGlobal.php
+++ src/Scopes/TenantScopeGlobal.php
@@ -6,6 +6,7 @@ namespace Revdojo\MT\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Revdojo\MT\Models\TenantBase;
 
 class TenantScopeGlobal implements Scope
 {
@@ -16,10 +17,15 @@ class TenantScopeGlobal implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
+        $tenant = TenantBase::where('id', config('tenancy.tenant_subdomain'))->first();
+        if (config('tenancy.tenant_subdomain') && !$tenant) {
+            return abort('403', 'Access denied. Tenant does not exist');
+        }
+
         if (!config('tenancy.tenant')) {
             return;
         }
-        
+
         $tenantIds = config('tenancy.tenant')->{$model->getTable()}->pluck('id')->toArray();
         $builder->whereIn('id', $tenantIds);
     }
