c317dcc929e204451b09d369623a73b66b4cb379
kylynangel15 <61867883+kylynangel15@users.noreply.github.com>
2023-11-28 19:47:40 +0800

    2.0.45 | trait boot fixed

--- src/Traits/Fillable.php
+++ src/Traits/Fillable.php
@@ -5,20 +5,8 @@ declare(strict_types=1);
 namespace Revdojo\MT\Traits;
 
 use Illuminate\Support\Facades\Schema;
-use Revdojo\MT\Helpers\GenerateHelper;
 trait Fillable
 {
-  
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            if (!$model->system_id) {
-                $model->system_id = GenerateHelper::generateSystemId(null, $model::class);
-            }
-        });
-    }
-
     public function getFillable()
     {
         return Schema::connection($this->getConnectionName())->getColumnListing($this->getTable());
--- src/Traits/TenantLink.php
+++ src/Traits/TenantLink.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Revdojo\MT\Traits;
 
 use  Revdojo\MT\Scopes\TenantScopeGlobal;
+use Revdojo\MT\Helpers\GenerateHelper;
 
 trait TenantLink
 {
@@ -12,5 +13,11 @@ trait TenantLink
     {
         parent::boot();
         static::addGlobalScope(new TenantScopeGlobal);
+
+        static::creating(function ($model) {
+            if (!$model->system_id) {
+                $model->system_id = GenerateHelper::generateSystemId(null, $model::class);
+            }
+        });
     }
 }
